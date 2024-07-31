a4b0557f69044e3ae0f098247ad4f80cef5d4487
Ben Morse <bjmorse@gmail.com>
2024-07-31 14:59:03 -0700

    run results

--- README.md
+++ README.md
@@ -9,3 +9,24 @@
 
 ## With resolution 1920x1080  (Same results)
 
+## With the tng sample
+The original video sample is 592x336
+this WORKS for
+
+* output_h264_avi.avi - works, but says unsupported codec
+* output_h265_mp4.mp4- works, but says unsupported codec
+* output_mpeg1.mpg - work
+* output_mpeg2.mpg - work
+* output_mpeg4.mp4 - work, no audio
+- output_xvid_avi.avi -works
+
+## Notes
+Things I want to try:
+the larger video down sampled to different rez
+set the bitrate and compare quality
+
+
+```
+rm /media/x/suvsd/output_*
+cp output_* /media/x/suvsd
+```
--- build.sh
+++ build.sh
@@ -3,18 +3,20 @@
 VIDIN=wd.sample.mkv
 VIDIN=tng.sample.avi
 
-ffmpeg -i ${VIDIN} -c:v libxvid -c:a mp3 output_xvid.avi
-ffmpeg -i ${VIDIN} -c:v mpeg2video -c:a mp2 output_mpeg2.mpg
-ffmpeg -i ${VIDIN} -c:v libx264 -c:a mp3 output_h264_avi.avi
+ffmpeg -i ${VIDIN} -c:v libxvid -c:a mp3 output_xvid_avi.avi
 ffmpeg -i ${VIDIN} -c:v mpeg1video -c:a mp2 output_mpeg1.mpg
-ffmpeg -i ${VIDIN} -c:v libvpx -c:a libvorbis output_vp8.mkv
-ffmpeg -i ${VIDIN} -c:v libvpx -c:a libvorbis output_vp8.webm
-ffmpeg -i ${VIDIN} -c:v libtheora -c:a libvorbis output_theora.ogv
-ffmpeg -i ${VIDIN} -c:v libx265 -c:a aac output_h265.mp4
-ffmpeg -i ${VIDIN} -c:v wmv1 -c:a wmav2 output_wmv1.asf
-ffmpeg -i ${VIDIN} -c:v libx265 -c:a libopus output_h265.mkv
-ffmpeg -i ${VIDIN} -c:v flv -c:a mp3 output_h263.flv
+ffmpeg -i ${VIDIN} -c:v mpeg2video -c:a mp2 output_mpeg2.mpg
 ffmpeg -i ${VIDIN} -c:v mpeg4 -c:a aac output_mpeg4.mp4
+ffmpeg -i ${VIDIN} -c:v libx264 -c:a mp3 output_h264_avi.avi
+ffmpeg -i ${VIDIN} -c:v libx265 -c:a aac output_h265_mp4.mp4
+
+# Not even recognized
+# ffmpeg -i ${VIDIN} -c:v libvpx -c:a libvorbis output_vp8.mkv
+# ffmpeg -i ${VIDIN} -c:v libvpx -c:a libvorbis output_vp8.webm
+# ffmpeg -i ${VIDIN} -c:v libtheora -c:a libvorbis output_theora.ogv
+# ffmpeg -i ${VIDIN} -c:v wmv1 -c:a wmav2 output_wmv1_asf.asf
+# ffmpeg -i ${VIDIN} -c:v libx265 -c:a libopus output_h265_mkv.mkv
+# ffmpeg -i ${VIDIN} -c:v flv -c:a mp3 output_h263_flv.flv
 # ffmpeg -i ${VIDIN} -c:v libaom-av1 -c:a libopus output_av1.mkv
 
 
